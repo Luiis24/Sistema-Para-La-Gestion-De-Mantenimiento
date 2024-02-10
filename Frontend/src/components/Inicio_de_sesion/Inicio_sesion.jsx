8b05b2048907a21f06e23b3fef0ef323c7c84e98
Luis <134552997+Luiis24@users.noreply.github.com>
2024-02-10 15:58:09 -0500

    Update Inicio_sesion.jsx
    Login Instructor

--- Frontend/src/components/Inicio_de_sesion/Inicio_sesion.jsx
+++ Frontend/src/components/Inicio_de_sesion/Inicio_sesion.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './inicio_sesion.css';
 
 export const Inicio_sesion = () => {
+  const [cc, setCc] = useState('');
+  const [password, setPassword] = useState('');
+
+  const Iniciar_Sesion = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/loginInstructor', {
+        cc_instructor: cc,
+        password_instructor: password
+      });
+
+      console.log(response.data); 
+
+    } catch (error) {
+      console.error('Error al iniciar sesión', error);
+    }
+  };
+
   return (
     <div className='todo'>
       <div className='complete'>
         <div className='inicio_sesion'>
-        <div className='titulo-sesion'>Iniciar Sesión</div>
-            <div className='numid'>Número de identificacion:</div>
-            <input className='inpt-cc' placeholder="Usuario" type="text" id="cc" name="cc" />
-            <div className='cont'>Contraseña:</div>
-            <input className='inpt-pass' type="password" id="pass" name="pass" placeholder="Contraseña"/>
-            <div className='btn-iniciar-ses'>
-             <button className='boton-iniciar' type='submit'>Ingresar</button>
-             </div>
+          <div className='titulo-sesion'>Iniciar Sesión</div>
+          <div className='numid'>Número de identificación:</div>
+          <input
+            className='inpt-cc'
+            placeholder="Usuario"
+            type="text"
+            value={cc}
+            onChange={(e) => setCc(e.target.value)}
+          />
+          <div className='cont'>Contraseña:</div>
+          <input
+            className='inpt-pass'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contraseña"
+          />
+          <div className='btn-iniciar-ses'>
+            <button className='boton-iniciar' type='button' onClick={Iniciar_Sesion}>
+              Ingresar
+            </button>
+          </div>
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
