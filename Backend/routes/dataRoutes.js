ab7445efb3dca76db87d2829e689b4618973c34b
Luis <134552997+Luiis24@users.noreply.github.com>
2024-02-10 15:58:44 -0500

    Update dataRoutes.js
    Login Ruta

--- Backend/routes/dataRoutes.js
+++ Backend/routes/dataRoutes.js
@@ -9,7 +9,7 @@ router.get('/instructores', dataController.getInstructores);
 router.post('/registerAprendiz', dataController.registerAprendiz);
 router.get('/aprendices', dataController.getAprendices);
 
-
+router.post('/loginInstructor', dataController.loginInstructor);
 
 
 module.exports = router;
