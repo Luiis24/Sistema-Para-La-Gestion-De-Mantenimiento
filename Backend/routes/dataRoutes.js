58a62cec832d331418b05d2208ac161446462c16
Luis <134552997+Luiis24@users.noreply.github.com>
2024-02-10 18:01:22 -0500

    Update dataRoutes.js

--- Backend/routes/dataRoutes.js
+++ Backend/routes/dataRoutes.js
@@ -4,12 +4,12 @@ const dataController = require('../controllers/dataControllers');
 
 
 router.post('/registerInstructor', dataController.registerInstructor);
+router.post('/loginInstructor', dataController.loginInstructor);
 router.get('/instructores', dataController.getInstructores);
 
 router.post('/registerAprendiz', dataController.registerAprendiz);
 router.get('/aprendices', dataController.getAprendices);
 
-router.post('/loginInstructor', dataController.loginInstructor);
-
+router.post('/registerHojaInspeccion', dataController.registerHojaInspeccion);
 
 module.exports = router;
