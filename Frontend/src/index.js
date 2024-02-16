7a4dfb639b9e213f4f6b05fd0bbcc8c32c357565
Luis <nandoarmo01@gmail.com>
2024-02-16 02:29:22 -0500

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -8,18 +8,21 @@ import { Menu_principal } from './components/Menu_principal/Menu_principal';
 import { Registro_aprendiz } from './components/Registro_aprendiz/Registro_aprendiz';
 import { Animacion_cuadros } from './components/Animacion_cuadros/Animacion_cuadros';
 import { Registro_instructor } from './components/Registro_instructor/Registro_instructor';
-import { HojaInspeccion } from './components/HojaInspeccion/HojaInspeccion'
 import { Componentes_check } from './components/Componentes_check/Componentes_check'
 import { Check_list } from './components/Check_list/Check_list'
+import Estado_componente from './components/Estado_componente/Estado_componente';
+import Crear_tipo_maquina from './components/Crear_tipo_maquina/Crear_tipo_maquina';
+import { Crear_maquina } from './components/Crear_maquina/Crear_maquina';
+import { Caracteristicas_motor } from './components/Caracteristicas_motor/Caracteristicas_motor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-  <Titulo_sena_cb/>
-  <Animacion_cuadros/>
-  <Componentes_check/>
-  <Check_list/>
+
+  <Crear_maquina/>
+  <Caracteristicas_motor/>
+
+
   </React.StrictMode>
 );
 
